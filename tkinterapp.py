b88c0ba8523e77a54011355d747c81af5ca17936
eugenia-vel <eugenia_vel@bk.ru>
2025-07-20 22:48:56 +0300

    added input

--- tkinterapp.py
+++ tkinterapp.py
@@ -6,11 +6,13 @@ root = Tk()
 root.title("Планирование финансов")
 root.geometry("{0}x{1}+0+0".format(root.winfo_screenwidth(), root.winfo_screenheight()))
 
-Label(text="Hello METANIT.COM").pack()
+Label(text="Выберите категории расходов: ").pack()
 categories = get_categories()
 temp = []
 for el in categories:
     temp.append(IntVar())
     ttk.Checkbutton(text=el, variable=temp[len(temp)-1]).pack()
     # ttk.Label(textvariable=temp[len(temp)-1]).pack()
+num = 0
+ttk.Entry().pack()
 root.mainloop()

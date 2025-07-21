bf89bb2445800e502235f26eaad8cf3eba0f346b
eugenia-vel <eugenia_vel@bk.ru>
2025-07-21 19:57:36 +0300

    added second page with statistics

--- funcs.py
+++ funcs.py
@@ -1,5 +1,6 @@
 from datetime import date, timedelta
 from openpyxl import load_workbook
+from tkinter import *
 
 def get_zero(num):
     if num < 10:
@@ -14,14 +15,58 @@ def get_week():
     last_day = first_day + timedelta(7)
     return get_zero(first_day.day)+ "." + get_zero(first_day.month) + " - " + get_zero(last_day.day) + "." + get_zero(last_day.month)
 
-def get_categories():
+def enter_categories(temp, categories, values, frame):
+    wb = load_workbook("categories.xlsx")
+    ws = wb.active
+    i = 1
+    for el in range (len(temp)):
+        print(categories[el], temp[el].get(), values[el].get())
+        if temp[el].get() == 1:
+            ws.cell(1, i, value=categories[el])
+            val = values[el].get()
+            try:
+                val = int(val)
+                ws.cell(2, i, value=val)
+            except:
+                Label(frame, text="Значения не должны содержать буквы").grid()
+                return 1
+            i += 1
+    frame.destroy()
+    print("works")
+    wb.save("categories.xlsx")
+
+def get_all_categories():
     return open("list of categories.txt", "r", encoding="utf-8").read().split(", ")
 
-    # print("выберите категории из списка:")
-    # for i in range(0, len(list)):
-    #     print(str(i) + ". " + list[i])
-    # return list(map(lambda x: int(x),input("введите номера категорий через запятую").split(",")))
+def get_chosen_categories():
+    categories = []
+    wb = load_workbook("categories.xlsx")
+    ws = wb.active
+    i = 1
+    while True:
+        val = ws.cell(row=1, column=i).value
+        i += 1
+        if val == None:
+            break
+        else:
+            categories.append(val)
+    return categories
 
-#     wb = load_workbook("categories.xlsx")
-#     ws = 
-#     for el in chosen_categories:
+def get_statistics(frame):
+    canvas = Canvas(bg="white", width=250, height=200)
+    canvas.grid()
+    for i in range(len(get_chosen_categories())):
+        wb = load_workbook("document.xlsx")
+        ws = wb.active
+        wasted = ws.cell(row=i+3, column=3).value
+        left = ws.cell(row=i+3, column=2).value
+        left_percent = left/(wasted + left)
+        print(left_percent)
+        Label(frame, text="Осталось").grid(row=i+1, column=0)
+        Label(frame, text=str(left)).grid(row=i+1, column=1)
+        Label(frame, text="Потрачено").grid(row=i+1, column=2)
+        Label(frame, text=str(wasted)).grid(row=i+1, column=3)
+        canvas.create_rectangle(25, 20+30*i, 25+200*left_percent, 40+30*i, fill="blue")
+        # rect_center_x = 
+        canvas.create_text(50, 100, text=("Осталось: "+str(left)))
+        canvas.create_rectangle(25+200*left_percent, 20+30*i, 225, 40+30*i, fill="red")
--- tkinterapp.py
+++ tkinterapp.py
@@ -1,18 +1,41 @@
 from tkinter import *
 from tkinter import ttk
 from funcs import *
+from openpyxl import *
+
+# def addEntry(row_num):
+#     entries[row_num - 1] = ttk.Entry()
+#     entries[row_num - 1].grid(frame1, row=row_num, column=1)
 
 root = Tk()
 root.title("Планирование финансов")
 root.geometry("{0}x{1}+0+0".format(root.winfo_screenwidth(), root.winfo_screenheight()))
-
-Label(text="Выберите категории расходов: ").pack()
-categories = get_categories()
-temp = []
-for el in categories:
-    temp.append(IntVar())
-    ttk.Checkbutton(text=el, variable=temp[len(temp)-1]).pack()
-    # ttk.Label(textvariable=temp[len(temp)-1]).pack()
-num = 0
-ttk.Entry().pack()
+wb = load_workbook("categories.xlsx")
+ws = wb.active
+if ws.cell(1,1).value==None:
+    frame1 = Frame(root, background='pink')
+    frame1.grid()
+    Label(frame1, text="Выберите категории расходов: ").grid(row=0, column=0)
+    categories = get_all_categories()
+    temp = []
+    entries = []
+    row_num = 1
+    for i in range(len(categories)):
+        temp.append(IntVar())
+        ttk.Checkbutton(frame1, text=categories[i], variable=temp[i]).grid(row=row_num, column=0)
+        entry = ttk.Entry(frame1)
+        entry.grid(row=row_num, column=1)
+        entries.append(entry)
+        row_num += 1
+        # ttk.Label(textvariable=temp[len(temp)-1]).pack()
+    btn = ttk.Button(frame1, text="Продолжить", command=lambda: enter_categories(temp, categories, entries, frame1))
+    btn.grid(row=row_num, column=0)
+    row_num += 1
+else:
+    frame1 = Frame(root, background='pink')
+    frame1.grid()
+    get_statistics(frame1)
+    Label(frame1, text="Rfeirihuvuo").grid(row=0, column=0)
+    btn = ttk.Button(frame1, text="Добавить новый расход")
+    btn.grid()
 root.mainloop()
